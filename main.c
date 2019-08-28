0440e6d811f0a661d1fde904ddeee46f65e95beb
John Mamish <john.mamish@gmail.com>
2019-08-27 23:57:37 -0700

    in this commit, main.c contains test code for the bit dispenser

--- main.c
+++ main.c
@@ -9,10 +9,33 @@
 
 #include "jpeg.h"
 #include "jpeg-requantizer.h"
+#include "bit_dispenser.h"
+
+void printbits_u8(uint8_t bits) {
+    for (int i = 0; i < 8; i++) {
+        if (bits & 0x80) {
+            printf("1");
+        } else {
+            printf("0");
+        }
+        bits <<= 1;
+    }
+}
+
+void printbits_u32(uint32_t bits) {
+    for (int i = 0; i < 32; i++) {
+        if (bits & 0x80000000) {
+            printf("1");
+        } else {
+            printf("0");
+        }
+        bits <<= 1;
+    }
+}
 
 int main(int argc, char** argv)
 {
-    if (argc != 2) {
+/*    if (argc != 2) {
         printf("needs an arg\n");
         return -1;
     }
@@ -22,6 +45,34 @@ int main(int argc, char** argv)
         printf("error reading jpeg\n");
         return -1;
     }
+*/
+
+    printf("testing bit dispenser\n");
+
+    uint8_t test_data[] = { 0x4a, 0xff, 0x37, 0x92 };
+
+    printf("test_data is ");
+    for (int i = 0; i < 4; i++) {
+        printbits_u8(test_data[i]);
+        printf(" ");
+    }
+    printf("\n");
+
+    uint32_t target = 0;
+    bit_dispenser_t* bd = bit_dispenser_create(test_data, 4);
+
+    for (int i = 0; i < 14; i++) {
+        bit_dispenser_dispense_u32(&target, 1, bd);
+    }
+
+    uint32_t target2 = 0;
+    bit_dispenser_dispense_u32(&target2, 13, bd);
+
+    printf("target1 = ");
+    printbits_u32(target);
+    printf("\ntarget2 = ");
+    printbits_u32(target2);
+    printf("\n");
 
     return 0;
 }
